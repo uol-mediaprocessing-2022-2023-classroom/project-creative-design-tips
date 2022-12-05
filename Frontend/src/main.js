ceb5668189004375070e2424c9b7fdbe72f47b9f
MaximilianSchütte <maximilian.schuette@uni-oldenburg.de>
2022-12-05 19:58:23 +0100

    this package is not working?!

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import MultiSelectAreasImage from 'multi-select-areas-image'
+
+Vue.use(MultiSelectAreasImage)
 
 Vue.config.productionTip = false
 
