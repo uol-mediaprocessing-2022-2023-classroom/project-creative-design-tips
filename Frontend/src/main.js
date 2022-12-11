02832b89dfdc6249a3957a43558f02d7fd8ae388
Maximilian Bosse <maex.max@gmail.com>
2022-12-12 00:18:54 +0100

    Prepping for index page

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
+require('./components/homepage.css');
+require('./components/button.css');
+
 Vue.config.productionTip = false
 
 new Vue({
