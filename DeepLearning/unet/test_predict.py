ed96245b75d73033cb2450fb984dddd46a19ee80
sebastian.diers <sebastian.diers@syntaxphoenix.com>
2023-01-24 00:22:08 +0100

    feat: New images

--- DeepLearning/unet/test_predict.py
+++ DeepLearning/unet/test_predict.py
@@ -3,7 +3,7 @@ from data import *
 
 mydata = dataProcess(512,512)
 
-imgs_test = mydata.load_test_data()
+imgs_test = mydata.load_test_data('pipeline/test.tif')
 
 myunet = myUnet(1)
 
@@ -15,9 +15,9 @@ imgs_mask_test = model.predict(imgs_test, verbose=1)
 
 np.save('imgs_mask_test.npy', imgs_mask_test)
 
-print("array to image")
 imgs = np.load('imgs_mask_test.npy')
 for i in range(imgs.shape[0]):
     img = imgs[i]
+
     img = array_to_img(img)
     img.save("test-predict/%d.jpg"%(i))
